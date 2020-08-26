e5df924fac861513679d8b08607e1ef94f4b0059
Reuben Smith <reuben-smith@hotmail.com>
2020-08-26 17:53:15 +0930

    Sets proper HTTP headers and implements rate limiting
    * Sets proper HTTP headers using Helmet middleware
    * Implements rate limiting against DoS attacks using express-rate-limit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import helmet from "helmet";
+import RateLimit from "express-rate-limit";
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
+
+// Helmet setup - helps you secure your Express apps by setting various HTTP headers.
+app.use(helmet());
+
+// Setup rate limiter
+const limiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes (mins * seconds * milliseconds)
+    max: 100, // Limit number of requests per IP
+    delayMs: 0 // Disables delays
+});
 
 // mongoose connection
 mongoose.Promise = global.Promise;
